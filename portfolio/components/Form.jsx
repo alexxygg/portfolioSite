9af44610fb65e9b1a12df45967372748dbaedd30
alexxygg <alexxygg@gmail.com>
2023-03-11 18:46:55 -0800

    Update Form.jsx

--- portfolio/components/Form.jsx
+++ portfolio/components/Form.jsx
@@ -15,6 +15,7 @@ function Form() {
           name="contact"
           method="POST"
         >
+          <input type="hidden" name="form-name" value="contact" />
           <div className="form-group">
             <label htmlFor="exampleInputName1" className="text-primary">
               Name
